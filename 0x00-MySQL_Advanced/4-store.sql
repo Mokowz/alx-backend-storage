3b606f6ef5dd44cbad09049a5562baca98829af0
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-08 09:06:31 +0000

    Make changes

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- create a trigger that will reduce the quantity of item
 CREATE TRIGGER reduce_quant
-AFTER INSERT ON order
+AFTER INSERT ON orders
 FOR EACH ROW
 UPDATE items SET quantity = quantity - NEW.number
 WHERE name=NEW.item_name;
