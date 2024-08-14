942469afc7b714c0df8d2412fe97d5f7e32aa243
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-14 20:15:55 +0300

    More files

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # List all documents
 
-import pymongo
-
 
 def list_all(mongo_collection):
   """ lists all documents in a collection """
-  cursor = db.mongo_collection.find()
+  cursor = mongo_collection.find()
 
   return cursor
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+# Insert a document in Python
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new doc"""
+    cursor = mongo_collection.insert_one(**kwargs)
+    return cursor.inserted_id
