44deaddd6c639a663dbe223dfb2bc43f37e68cc8
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-14 21:37:09 +0300

    Read from redis and return original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Write strings to redis"""
 import redis
+import sys
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 UnionMore = Union[str, bytes, int, float]
@@ -20,3 +21,19 @@ class Cache:
         rand_key = str(uuid.uuid4())
         self._redis.mset({rand_key: data})
         return rand_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> UnionMore:
+        """COnvert to binary"""
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
+
+    def get_str(self: bytes) -> int:
+        """Strings"""
+        return int.from_bytes(self, sys.byteorder)
+
+    def get_int(self: bytes) -> int:
+        """Ints"""
+        return self.decode("utf-8")
