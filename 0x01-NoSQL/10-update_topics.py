17c5a8eb282366e20770811d8c2358c9c9468adb
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-14 20:45:07 +0300

    Complete the additional tasks

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Update all topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update all topics of a document"""
+    doc = mongo_collection.update_one({"name": name},
+                                      {"$set": {"topics": topics}})
+
+    return doc
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""School by topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    cursor = mongo_collection.find({"topics": topic})
+    return cursor
